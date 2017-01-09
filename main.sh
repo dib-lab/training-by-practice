564cd4a82aaa3a6e3d0c4180a386cfe8a4e7fd0b
drtamermansour <drtamermansour@gmail.com>
2017-01-09 17:39:25 -0500

    tasks4-5

--- main.sh
+++ main.sh
@@ -18,3 +18,9 @@ rm hg19.coding.bed
 ## Task3: extract transcripts with 3 exons from the sorted file from task2. Name the new file "hg19.coding_subset.3exon.sorted.bed" 
 
 
+## Task4: extract transcripts with 3 exons and chr17 from the sorted file from task2. Name the new file "hg19.coding_subset.3exon.chr17.sorted.bed" 
+
+
+## Task5: starting with the original input file, make a new column that has the length of transcript in the genome. For examle if it starts at 1 and ends at 500 then its length would be 499. Save the new file under the name "hg19.coding_subset.length.bed"
+
+
